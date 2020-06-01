7d34cd4b38f54ea5ecc1cac0db293a31e05ed9b6
Thomas Hudson <g.t.hudson@durham.ac.uk>
2020-06-01 17:09:33 +0100

    Fixed multiball issues

--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -12,10 +12,9 @@ import time
 import os
 import math
 from sklearn.model_selection._search import ParameterSampler, ParameterGrid
-#from utils import ParameterSamplerNoReset
 import numpy as np
 import subprocess
-
+import random
 from simulate import simulate
 
 import argparse
@@ -41,7 +40,7 @@ per_ball_parameter_space = {
 
 # Global parameters
 parameter_space = {
-    "num_balls":range(1,3),
+    "num_balls":range(1,4),
 #    "gx":[0.1*i for i in range(0,5)],
 #    "gy":[0.1*i for i in range(0,5)],
     "background_color":['#{0:x}{0:x}{0:x}'.format(x) for x in range(0,16)],
@@ -68,116 +67,121 @@ config["number_of_simulations"] = args.number_of_simulations
 with open(os.path.join(args.output_path,"config.yml"),'w') as f:
     yaml.dump(config,f)
 
-rng = np.random.RandomState(0)
+rng = np.random.RandomState(42)
+random.seed(42)
 
+# Create combined parameter space (expand by max_num_balls
+print("Building space...")
+for key in per_ball_parameter_space:
+    for i in range(max(list(parameter_space["num_balls"]))):
+        parameter_space[key+str(i)] = per_ball_parameter_space[key]
+print("Done")
 
 # Loop through param space
 # Run simulate gen
 # Render
-i = 0
+simulation_num = 0
 balls = []
+sampler = ParameterGrid(parameter_space)
 
-# TODO: Find a more elegant way of doing this
-#   - Currently this just makes a large list of possible balls
-#   - This is because ParameterSampler needs to be run in a loop and the rnd state doesn't update to there is no variety
-#   - This could be fixed using a custom ParameterSampler which maintains an updated random state
-for ball in ParameterSampler(per_ball_parameter_space,max(list(parameter_space["num_balls"]))*args.number_of_simulations*10,random_state=rng):
-    balls.append(ball)
+while simulation_num < args.number_of_simulations:
+    random_idx = random.randint(0, len(sampler)-1)
+    params = sampler[random_idx]
 
-for params in ParameterSampler(parameter_space,9999999,random_state=rng):
-#for params in ParameterGrid(parameter_space):
-
-    if i >= args.number_of_simulations:
-        break
-
-    ball_params = []
     good_simulation = True
-    while len(ball_params) < params["num_balls"]:
+    for i in range(params["num_balls"]):
 
         # Check if too close to boundary
-        ball_data = balls.pop()
-        if ball_data["radius"] > ball_data["x"] or \
-           ball_data["radius"] > ball_data["y"] or \
-           ball_data["radius"] > (1-ball_data["x"]) or \
-           ball_data["radius"] > (1-ball_data["y"]):
+        if params["radius"+str(i)] > params["x"+str(i)] or \
+           params["radius"+str(i)] > params["y"+str(i)] or \
+           params["radius"+str(i)] > (1-params["x"+str(i)]) or \
+           params["radius"+str(i)] > (1-params["y"+str(i)]):
             print("Rejecting: Too close to boundary!")
-            continue
+            good_simulation = False
+            break
 
         # Check if too close to other balls
         good_ball = True
-        for existing_ball in ball_params:
-            distance_between_balls = math.sqrt((ball_data["x"]-existing_ball["x"])**2 + (ball_data["y"] - existing_ball["y"]) **2)
-            if distance_between_balls < existing_ball["radius"]+ball_data["radius"]:
+        for j in range(i):
+            distance_between_balls = math.sqrt((params["x"+str(i)]-params["x"+str(j)])**2 + (params["y"+str(i)] - params["y"+str(j)]) **2)
+            if distance_between_balls < params["radius"+str(j)]+params["radius"+str(i)]:
                 print("Rejecting: Too close to other balls!")
                 good_ball = False
                 break
         if not good_ball:
-            continue
+            good_simulation = False
+            break
 
         # Check if ball colour same as background colour
-        if params["background_color"] == ball_data["foreground_color"]:
+        if params["background_color"] == params["foreground_color"+str(i)]:
             print("Rejecting: Colors match!")
-            continue
-        ball_params.append(ball_data.copy())
+            good_simulation = False
+            break
 
-    # Simulate
-    per_ball_data = {}
-    for data in ball_params:
-        for k in data.keys():
-            per_ball_data[k] = per_ball_data.get(k,[]) + [data[k]]
-    params = {**params,**per_ball_data}
-    print("Simulation",i,params)
-    timesteps = simulate(num_timesteps_per_simulation,**params)
+        # Add to lists
+        for key in per_ball_parameter_space.keys():
+            params[key] = params.get(key,[]) + [params[key+str(i)]]
 
-    # Make directory
-    formatted_name = "0"*(len(str(args.number_of_simulations))-len(str(i)))+str(i)
-    formatted_name = os.path.join(args.output_path,formatted_name)
-    os.mkdir(formatted_name)
+    # Remove old keys
+    for i in range(max(list(parameter_space["num_balls"]))):
+        for key in per_ball_parameter_space.keys():
+            del params[key+str(i)]
 
-    # Write positions.csv file
-    with open(os.path.join(formatted_name,"positions.csv"),'w') as f:
-        header = "timestep,"\
-            +",".join(["x"+str(i) for i in range(params["num_balls"])])+","\
-            +",".join(["y"+str(i) for i in range(params["num_balls"])])
+    if good_simulation:
+        print("Simulation",simulation_num,params)
+        print()
+        print()
+        timesteps = simulate(num_timesteps_per_simulation,**params)
 
-        f.write(header+"\n")
+        # Make directory
+        formatted_name = "0"*(len(str(args.number_of_simulations))-len(str(simulation_num)))+str(simulation_num)
+        formatted_name = os.path.join(args.output_path,formatted_name)
+        os.mkdir(formatted_name)
+
+        # Write positions.csv file
+        with open(os.path.join(formatted_name,"positions.csv"),'w') as f:
+            header = "timestep,"\
+                +",".join(["x"+str(i) for i in range(params["num_balls"])])+","\
+                +",".join(["y"+str(i) for i in range(params["num_balls"])])
+
+            f.write(header+"\n")
+            for t in timesteps:
+                line = ""
+                for item in t:
+                    if isinstance(item,list):
+                        line += ","+",".join([str(it) for it in item])
+                    else:
+                        line += ","+str(item)
+                f.write(",".join(line+"\n"))
+
+        # Render the images
         for t in timesteps:
-            line = ""
-            for item in t:
-                if isinstance(item,list):
-                    line += ","+",".join([str(it) for it in item])
+            filename = os.path.join(formatted_name,"frame_"+"{:03d}".format(t[0])+".png")
+            for j in range(len(t[1])):
+                base = os.path.dirname(__file__)
+                if base == "":
+                    base = "./"
+                cmd = os.path.join(base,"draw_circle.sh")
+
+                if j == 0:
+                    # First ball - pick background colour
+                    cmd += " -b '"+params["background_color"]+"'"
                 else:
-                    line += ","+str(item)
-            f.write(",".join(line+"\n"))
+                    # Other balls - use image of previous ball as background image
+                    cmd += " -i '"+filename+"'"
 
-    # Render the images
-    for t in timesteps:
-        filename = os.path.join(formatted_name,"frame_"+"{:03d}".format(t[0])+".png")
-        for j in range(len(t[1])):
-            base = os.path.dirname(__file__)
-            if base == "":
-                base = "./"
-            cmd = os.path.join(base,"draw_circle.sh")
+                cmd += " -r "+str(int(params["radius"][j]*image_size))
+                cmd += " -f '"+params["foreground_color"][j]+"'"
+                cmd += " "+str(round(t[1][j]*image_size))
+                cmd += " "+str(round(t[2][j]*image_size))
+                cmd += " "+filename
+                os.system(cmd)
 
-            if j == 0:
-                # First ball - pick background colour
-                cmd += " -b '"+params["background_color"]+"'"
-            else:
-                # Other balls - use image of previous ball as background image
-                cmd += " -i '"+filename+"'"
+        # Add metadata
+        with open(os.path.join(formatted_name,"config.yml"),'w') as f:
+            params["date"] = time.time()
+            yaml.dump(params,f)
 
-            cmd += " -r "+str(int(params["radius"][j]*image_size))
-            cmd += " -f '"+params["foreground_color"][j]+"'"
-            cmd += " "+str(round(t[1][j]*image_size))
-            cmd += " "+str(round(t[2][j]*image_size))
-            cmd += " "+filename
-            os.system(cmd)
-
-    # Add metadata
-    with open(os.path.join(formatted_name,"config.yml"),'w') as f:
-        params["date"] = time.time()
-        yaml.dump(params,f)
-
-    # Just for preview - make a gif
-    os.system("convert -delay 20 "+formatted_name+"/*.png "+formatted_name+"/simulation.gif")
-    i+=1
+        # Just for preview - make a gif
+        os.system("convert -delay 20 "+formatted_name+"/*.png "+formatted_name+"/simulation.gif")
+        simulation_num+=1
